09b6318ea9cdb6f5bb115dc785d664691460fe4e
mpijierro <mpijierro@gmail.com>
2017-02-23 18:09:21 +0100

    add rule for laravel validation

--- src/Identity.php
+++ src/Identity.php
@@ -24,12 +24,7 @@ class Identity
             return ($letras[(substr($nif, 0, 8) % 23)] == $nif[8]);
         } else {
             if (preg_match($nieRegEx, $nif)) {
-                /**
-                 * Los NIE's de extranjeros residentes en España tienen una letra (X, Y, Z), 7 números y dígito de control.
-                 * Para el cálculo del dígito de control se sustituye:
-                 * X =0
-                 * Y =1
-                 * Z = 2 y se aplica el mismo algoritmo que para el NIF.*/
+
                 $r = str_replace(['X', 'Y', 'Z'], [0, 1, 2], $nif);
 
                 return ($letras[(substr($r, 0, 8) % 23)] == $nif[8]);
--- src/IdentityServiceProvider.php
+++ src/IdentityServiceProvider.php
@@ -3,6 +3,8 @@
 namespace MPijierro\Identity;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Validator;
+use MPijierro\Identity\Identity;
 
 class IdentityServiceProvider extends ServiceProvider
 {
@@ -13,6 +15,58 @@ class IdentityServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+
+        Validator::extend('nif', function ($attribute, $value, $parameters, $validator) {
+            $identity = new \MPijierro\Identity\Identity();
+
+            return $identity->isValidNif($value);
+        });
+
+        Validator::extend('cif', function ($attribute, $value, $parameters, $validator) {
+            $identity = new \MPijierro\Identity\Identity();
+
+            return $identity->isValidCif($value);
+        });
+
+        Validator::extend('nie', function ($attribute, $value, $parameters, $validator) {
+            $identity = new \MPijierro\Identity\Identity();
+
+            return $identity->isValidNie($value);
+        });
+
+    }
+
+
+    private function addRules()
+    {
+
+        $this->addRules();
+
+        $this->addMessages();
+
+    }
+
+
+    private function addMessages()
+    {
+        Validator::replacer('nif', function ($message, $attribute, $rule, $parameters) {
+
+            return "The $attribute field is not a valid NIF.";
+
+        });
+
+        Validator::replacer('cif', function ($message, $attribute, $rule, $parameters) {
+
+            return "The $attribute field is not a valid CIF.";
+
+
+        });
+
+        Validator::replacer('nie', function ($message, $attribute, $rule, $parameters) {
+
+            return "The $attribute field is not a valid NIE.";
+
+        });
 
     }
 
