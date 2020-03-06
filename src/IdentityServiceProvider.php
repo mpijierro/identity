a934a5a3b07127cd85a587453b0c629df1a45f58
Marc Mascort Bou <marc@articstudio.com>
2020-03-06 19:48:46 +0100

    Improve Service Provider implementation

--- src/IdentityServiceProvider.php
+++ src/IdentityServiceProvider.php
@@ -2,96 +2,13 @@
 
 namespace MPijierro\Identity;
 
-use Illuminate\Support\ServiceProvider;
+use Illuminate\Contracts\Support\DeferrableProvider;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\ServiceProvider;
 use MPijierro\Identity\Identity;
 
-class IdentityServiceProvider extends ServiceProvider
+class IdentityServiceProvider extends ServiceProvider implements DeferrableProvider
 {
-    /**
-     * Bootstrap the application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-
-        $this->addRules();
-
-        $this->addMessages();
-
-    }
-
-
-    private function addRules()
-    {
-
-        Validator::extend('nif', function ($attribute, $value, $parameters, $validator) {
-            $identity = new \MPijierro\Identity\Identity();
-
-            return $identity->isValidNif($value);
-        });
-
-        Validator::extend('cif', function ($attribute, $value, $parameters, $validator) {
-            $identity = new \MPijierro\Identity\Identity();
-
-            return $identity->isValidCif($value);
-        });
-
-        Validator::extend('nie', function ($attribute, $value, $parameters, $validator) {
-            $identity = new \MPijierro\Identity\Identity();
-
-            return $identity->isValidNie($value);
-        });
-
-        Validator::extend('iban', function ($attribute, $value, $parameters, $validator) {
-            $identity = new \MPijierro\Identity\Identity();
-
-            return $identity->isValidIban($value);
-        });
-
-        Validator::extend('nnss', function ($attribute, $value, $parameters, $validator) {
-            $identity = new \MPijierro\Identity\Identity();
-
-            return $identity->isValidNNSS($value);
-        });
-    }
-
-
-    private function addMessages()
-    {
-        Validator::replacer('nif', function ($message, $attribute, $rule, $parameters) {
-
-            return "The $attribute field is not a valid NIF.";
-
-        });
-
-        Validator::replacer('cif', function ($message, $attribute, $rule, $parameters) {
-
-            return "The $attribute field is not a valid CIF.";
-
-
-        });
-
-        Validator::replacer('nie', function ($message, $attribute, $rule, $parameters) {
-
-            return "The $attribute field is not a valid NIE.";
-
-        });
-
-        Validator::replacer('iban', function ($message, $attribute, $rule, $parameters) {
-
-            return "The $attribute field is not a valid IBAN.";
-
-        });
-
-        Validator::replacer('nnss', function ($message, $attribute, $rule, $parameters) {
-
-            return "The $attribute field is not a valid Social Security  Number.";
-
-        });
-
-    }
 
     /**
      * Register the application services.
@@ -100,10 +17,100 @@ class IdentityServiceProvider extends ServiceProvider
      */
     public function register()
     {
-
-        \App::bind('Identity', function () {
+        $this->app->bind(Identity::class, function () {
             return new Identity();
         });
+        $this->app->alias(
+            Identity::class,
+            'Identity'
+        );
+    }
+    
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->addRules();
+        $this->addMessages();
 
+    }
+
+    /**
+     * Add rules
+     * 
+     * @return void
+     */
+    private function addRules(): void
+    {
+
+        Validator::extend('nif', function ($attribute, $value, $parameters, $validator) {
+            $identity = new \MPijierro\Identity\Identity();
+            return $identity->isValidNif($value);
+        });
+
+        Validator::extend('cif', function ($attribute, $value, $parameters, $validator) {
+            $identity = new \MPijierro\Identity\Identity();
+            return $identity->isValidCif($value);
+        });
+
+        Validator::extend('nie', function ($attribute, $value, $parameters, $validator) {
+            $identity = new \MPijierro\Identity\Identity();
+            return $identity->isValidNie($value);
+        });
+
+        Validator::extend('iban', function ($attribute, $value, $parameters, $validator) {
+            $identity = new \MPijierro\Identity\Identity();
+            return $identity->isValidIban($value);
+        });
+
+        Validator::extend('nnss', function ($attribute, $value, $parameters, $validator) {
+            $identity = new \MPijierro\Identity\Identity();
+            return $identity->isValidNNSS($value);
+        });
+    }
+
+    /**
+     * Add messages
+     * 
+     * @return void
+     */
+    private function addMessages(): void
+    {
+        Validator::replacer('nif', function ($message, $attribute, $rule, $parameters) {
+            return "The $attribute field is not a valid NIF.";
+        });
+
+        Validator::replacer('cif', function ($message, $attribute, $rule, $parameters) {
+            return "The $attribute field is not a valid CIF.";
+        });
+
+        Validator::replacer('nie', function ($message, $attribute, $rule, $parameters) {
+            return "The $attribute field is not a valid NIE.";
+        });
+
+        Validator::replacer('iban', function ($message, $attribute, $rule, $parameters) {
+            return "The $attribute field is not a valid IBAN.";
+        });
+
+        Validator::replacer('nnss', function ($message, $attribute, $rule, $parameters) {
+            return "The $attribute field is not a valid Social Security  Number.";
+        });
+
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [
+            Identity::class,
+            'Identity'
+        ];
     }
 }
