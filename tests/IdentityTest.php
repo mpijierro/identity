35fa90eefcc3211803afb6ed38f42d1e5b57e70c
Marc Mascort Bou <marc@articstudio.com>
2020-03-06 19:44:50 +0100

    Upgrade dependencies

--- tests/IdentityTest.php
+++ tests/IdentityTest.php
@@ -3,8 +3,9 @@
 namespace MPijierro\Identity\tests;
 
 use MPijierro\Identity\Identity;
+use PHPUnit\Framework\TestCase;
 
-class IdentityTest extends \PHPUnit\Framework\TestCase
+class IdentityTest extends TestCase
 {
 
     /**
@@ -13,20 +14,20 @@ class IdentityTest extends \PHPUnit\Framework\TestCase
     private $identity;
 
 
-    public function setUp()
+    public function setUp(): void
     {
         $this->identity = new Identity();
     }
 
 
-    public function test_sanitize()
+    public function test_sanitize(): void
     {
         $this->assertEquals('Q6887124C', $this->identity->sanitize('q6887124c'));
         $this->assertEquals('Q6887124C', $this->identity->sanitize('q6887124C    '));
         $this->assertEquals('000Q6887124C', $this->identity->sanitize('000q6887124C    '));
     }
 
-    public function test_valid_cif()
+    public function test_valid_cif(): void
     {
         $this->assertTrue($this->identity->isValidCif('Q6887124C'));
         $this->assertTrue($this->identity->isValidCif('J51062271'));
@@ -34,7 +35,7 @@ class IdentityTest extends \PHPUnit\Framework\TestCase
         $this->assertTrue($this->identity->isValidCif('N8796829C'));
     }
 
-    public function test_invalid_cif()
+    public function test_invalid_cif(): void
     {
         $this->assertFalse($this->identity->isValidCif('Q6887124'));
         $this->assertFalse($this->identity->isValidCif('Q6887123C'));
@@ -45,7 +46,7 @@ class IdentityTest extends \PHPUnit\Framework\TestCase
         $this->assertFalse($this->identity->isValidCif(null));
     }
 
-    public function test_valid_nif()
+    public function test_valid_nif(): void
     {
         $this->assertTrue($this->identity->isValidNif('21361012S'));
         $this->assertTrue($this->identity->isValidNif('64160547T'));
@@ -53,7 +54,7 @@ class IdentityTest extends \PHPUnit\Framework\TestCase
         $this->assertTrue($this->identity->isValidNif('08861617Q'));
     }
 
-    public function test_invalid_nif()
+    public function test_invalid_nif(): void
     {
         $this->assertFalse($this->identity->isValidNif('21361012'));
         $this->assertFalse($this->identity->isValidNif('1361012S'));
@@ -69,14 +70,14 @@ class IdentityTest extends \PHPUnit\Framework\TestCase
     }
 
 
-    public function test_valid_nie()
+    public function test_valid_nie(): void
     {
         $this->assertTrue($this->identity->isValidNie('X3212050P'));
         $this->assertTrue($this->identity->isValidNie('X2792997S'));
     }
 
 
-    public function test_invalid_nie()
+    public function test_invalid_nie(): void
     {
         $this->assertFalse($this->identity->isValidNie('Z8930474'));
         $this->assertFalse($this->identity->isValidNie('8930474Q'));
@@ -90,7 +91,7 @@ class IdentityTest extends \PHPUnit\Framework\TestCase
     }
 
 
-    public function test_document_is_any_valid()
+    public function test_document_is_any_valid(): void
     {
 
         $this->assertTrue($this->identity->documentIsValid('21361012S'));
@@ -103,7 +104,7 @@ class IdentityTest extends \PHPUnit\Framework\TestCase
     }
 
 
-    public function test_valid_iban()
+    public function test_valid_iban(): void
     {
         $this->assertTrue($this->identity->isValidIban('ES9100490013112991609374'));
         $this->assertTrue($this->identity->isValidIban('ES09 2038-0626-0160-0002-5280'));
@@ -115,7 +116,7 @@ class IdentityTest extends \PHPUnit\Framework\TestCase
     }
 
 
-    public function test_invalid_iban()
+    public function test_invalid_iban(): void
     {
 
         $this->assertFalse($this->identity->isValidIban('ES9100490013112991609374a'));
@@ -129,7 +130,7 @@ class IdentityTest extends \PHPUnit\Framework\TestCase
 
     }
 
-    public function test_valid_nnss()
+    public function test_valid_nnss(): void
     {
 
         $this->assertTrue($this->identity->isValidNNSS('307365535155'));
@@ -137,7 +138,7 @@ class IdentityTest extends \PHPUnit\Framework\TestCase
 
     }
 
-    public function test_invalid_nnss()
+    public function test_invalid_nnss(): void
     {
 
         $this->assertFalse($this->identity->isValidNNSS('107819249351rtrtr'));
